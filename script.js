b7fb42bd91de90d7117474ab5c782bf26442b24b
cagan <cagannergis@gmail.com>
2026-01-01 21:59:00 +0300

    Delete btn eklendi

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const addBtn = document.getElementById("add-button");
 const taskInput = document.getElementById("task-input");
 const taskList = document.getElementById("task-list");
+const li = document.createElement("li");
 
 addBtn.addEventListener("click", () => {
-    const taskText = taskInput.value;
+    const taskText = taskInput.value.trim();
     if (taskText) {
         const li = document.createElement("li");
         li.textContent = taskText;
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        deleteBtn.id = "delete-btn";
+        deleteBtn.addEventListener("click", () => {
+            li.remove();
+        });
+
+        li.appendChild(deleteBtn);
         taskList.appendChild(li);
         taskInput.value = "";
     }
 });
+
 
